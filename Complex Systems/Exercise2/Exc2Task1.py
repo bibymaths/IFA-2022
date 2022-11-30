6a43659f60945af57b8db2b9399fa32e219ed99f
maikeherken <maikeherken@mi.fu-berlin.de>
2022-11-30 14:18:54 +0000

    Upload New File

---
+++ Complex Systems/Exercise2/Exc2Task1.py
@@ -0,0 +1,86 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def ssa(s, k, x_0, tFinal):
+    # initialize lists
+    times = []
+    states = []
+    t = 0.0
+    x = x_0
+    times.append(t)
+    states.append(x[1])
+
+    while t <= tFinal:
+        # When?  
+        R = ReactionRates(k,x)
+        lambdaVar = np.sum(R)
+        tau = 1/lambdaVar * np.log(1/np.random.random())      
+
+        # End time reached
+        if t + tau > tFinal:
+            t = tFinal
+            break
+        
+        # Update time
+        t = t + tau
+
+        # What reaction happens
+        goal = lambdaVar * np.random.random()
+        currentSum = 0
+        j = 0
+        for i, rate in enumerate(R):
+            if(currentSum + rate > goal):
+                break
+            currentSum+=rate
+            j = i
+
+
+        # Update states
+        
+        x += s[:,j]
+        times.append(t)
+        states.append(x[1])
+
+    return times, states
+
+
+def ReactionRates(k,X):
+        R = np.zeros((6,1))
+        R[0] = k[0]
+        R[1] = k[1]*X[0]
+        R[2] = k[2]*X[0]*X[1]
+        R[3] = k[1]*X[1]*3*10**7
+        R[4] = k[3]*X[1]
+        R[5] = k[1]*X[2]
+        return R
+
+
+inputFile = np.loadtxt('Input.txt')
+np.random.seed(seed=int(inputFile[0]))
+nrSimulations = int(inputFile[1])
+
+# Stoichiometric matrix
+s = np.array([[1,-1,-1,0,0,0],[0,0,1,-1,-1,0],[0,0,0,0,1,-1],[0,0,0,1,0,0]])
+
+# Reaction parameters
+lamda = 1*10**(-4)
+delta = 1*10**(-8)
+beta = 5*10**(-5)
+kr = 0.3
+
+k = [lamda, delta, beta, kr]
+
+# Initial state
+x_0 =[lamda/delta, 20, 0, 0]
+tFinal = 10
+
+for i in range(nrSimulations):
+    states, times = ssa(s, k, x_0, tFinal)
+
+    plt.plot(states, times)
+    plt.show()
+    output = np.concatenate((np.array(times,ndmin=2),np.array(states,ndmin=2)), axis=0)
+    np.savetxt('traj'+str(i+1)+'.txt',output,delimiter = ',',fmt='%1.3f')
+    print('files saved')
+    
+
