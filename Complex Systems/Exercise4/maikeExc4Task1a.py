75beec2c0167f06e8463ed33075a4d8ca1e5f62a
maikeherken <maikeherken@mi.fu-berlin.de>
2022-12-12 17:05:54 +0000

    Replace maikeExc4Task1a.py

--- Complex Systems/Exercise4/maikeExc4Task1a.py
+++ Complex Systems/Exercise4/maikeExc4Task1a.py
@@ -2,7 +2,9 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import optimize, integrate
+from scipy.optimize import curve_fit 
+from scipy.integrate import odeint
+
 
 # Computes reaction rates with unknown parameters [c2,c5]
 def propensities(X, c2, c3, c4, c5):
@@ -21,12 +23,12 @@ def RHS(t, X, c2, c3, c4, c5):
     return dX.flatten('F')
 
 def ModelPrediction(t_data, c2,c3,c4,c5):
-    y = integrate.odeint(RHS, X0.flatten('F'), t_data, args=(c2,c3,c4,c5),tfirst=True)
+    y = odeint(RHS, X0.flatten('F'), t_data, args=(c2,c3,c4,c5),tfirst=True)
     return y[:,2]
 
 # Stoichiometric matrix
 global s, k, X0
-s = np.array([[1,-1,-1,0,0,0],[0,0,1,-1,1,0],[0,0,-1,0,0,-1]])
+s = np.array([[1,-1,-1,0,0,0],[0,0,1,-1,0,0],[0,0,-1,0,1,-1]])
 
 # Reaction parameters
 k0 = 100
@@ -40,15 +42,23 @@ X0 = np.array([k0/k1, 0, 20])
 t_data, y_data = np.load('Data.npy')
 
 # Sumbmit with first line uncommented and second line commented
-popt, pcov = optimize.curve_fit(ModelPrediction, t_data,y_data, bounds=(0,np.inf))
-#popt, pcov = optimize.curve_fit(ModelPrediction, t_data, y_data.copy(), bounds=(0,np.inf), p0=[0.1,1,5,1])
+#popt, pcov = curve_fit(ModelPrediction, t_data,y_data, bounds=(0,np.inf))
+popt, pcov = curve_fit(ModelPrediction, t_data, y_data, bounds=(0,np.inf), p0=[0.1,1,5,1])
 np.savetxt('Params.txt',popt,delimiter = ',',fmt='%1.2f')
 
 y_sim = ModelPrediction(t_data, popt[0], popt[1], popt[2], popt[3])
+y_reference = ModelPrediction(t_data, 0.0, 0.65, 40.18, 13.3)
 
-print('Diff from ref: \n{},\n{},\n{},\n{}'.format(popt[0]-0.0,popt[1]-0.65,popt[2]-10.18,popt[3]-13.3))
+print([ "{:1.2f}".format(x) for x in popt ])
+print('Diff from ref: \n{},\n{},\n{},\n{}'.format(popt[0]-0.0,popt[1]-0.65,popt[2]-40.18,popt[3]-13.3))
 
-plt.plot(t_data, y_data, label = 'Virus concentration')
+
+plt.figure(figsize=(8,6))
+plt.plot(t_data, y_data,marker = 's', label = 'Virus concentration')
 plt.plot(t_data, y_sim, label='simulation')
+plt.plot(t_data, y_reference, label='reference params')
+plt.ylabel('Number Viruses')
+plt.xlabel('Time')
 plt.legend()
+plt.savefig('Virus Concentrations.pdf', bbox_inches='tight')
 plt.show()
