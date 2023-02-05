0bf4916b4d2eb8bd02ff2797de2b4cdf28bc9831
Maike Herkenrath <maikeherken@compute11.imp.fu-berlin.de>
2023-02-05 20:23:25 +0100

    partial solution for pigeon hole fmindex search

--- Advanced Algorithms/ImplementingSearch/src/fmindex_pigeon_search.cpp
+++ Advanced Algorithms/ImplementingSearch/src/fmindex_pigeon_search.cpp
@@ -7,11 +7,39 @@
 #include <seqan3/search/fm_index/fm_index.hpp>
 #include <seqan3/search/search.hpp>
 
+std::vector<std::vector<seqan3::dna5>> partition(std::vector<seqan3::dna5> &query, int k){
+    int part_len = query.size()/(k+1);
+    seqan3::debug_stream << "part len:" << part_len << "\n";
+    std::vector<seqan3::dna5>::const_iterator start;
+    std::vector<seqan3::dna5>::const_iterator end;
+    std::vector<std::vector<seqan3::dna5>> parts(k+1);
+
+    seqan3::debug_stream << "query: " << query << "\n";
+
+    for (int i = 0; i < k; i++){
+        start = query.begin() + part_len * i;
+        end = query.begin() + part_len * (i+1);
+        parts[i] = std::vector<seqan3::dna5> (start, end);
+        //seqan3::debug_stream << "part " << i << ": " << parts[i] << "\n";
+    }
+
+    start = query.begin() + part_len * k;
+    end = query.begin() + query.size();   //(k*part_len);
+    parts[k] = std::vector<seqan3::dna5> (start, end);
+    //seqan3::debug_stream << "part k+1: " << parts[k] << "\n";
+
+    return parts;
+}
+
 int main(int argc, char const* const* argv) {
     seqan3::argument_parser parser{"fmindex_pigeon_search", argc, argv, seqan3::update_notifications::off};
 
     parser.info.author = "SeqAn-Team";
     parser.info.version = "1.0.0";
+
+    
+    auto reference_file = std::filesystem::path{};
+    parser.add_option(reference_file, '\0', "reference", "path to the reference file");
 
     auto index_path = std::filesystem::path{};
     parser.add_option(index_path, '\0', "index", "path to the query file");
@@ -27,7 +55,17 @@ int main(int argc, char const* const* argv) {
     }
 
     // loading our files
+    auto reference_stream = seqan3::sequence_file_input{reference_file};
     auto query_stream     = seqan3::sequence_file_input{query_file};
+
+    // read reference into memory
+    // Attention: we are concatenating all sequences into one big combined sequence
+    //            this is done to simplify the implementation of suffix_arrays
+    std::vector<seqan3::dna5> reference;
+    for (auto& record : reference_stream) {
+        auto r = record.sequence();
+        reference.insert(reference.end(), r.begin(), r.end());
+    }
 
     // read query into memory
     std::vector<std::vector<seqan3::dna5>> queries;
@@ -51,7 +89,6 @@ int main(int argc, char const* const* argv) {
     //!TODO here adjust the number of searches
     queries.resize(100); // will reduce the amount of searches
 
-
     //!TODO !ImplementMe use the seqan3::search to find a partial error free hit, verify the rest inside the text
     // Pseudo code (might be wrong):
     // for query in queries:
@@ -60,6 +97,21 @@ int main(int argc, char const* const* argv) {
     //          for (pos in search(index, part[p]):
     //              if (verify(ref, query, pos +- ....something)):
     //                  std::cout << "found something\n"
+
+    for (auto && q : queries){
+        // partitioning the query in 3 parts
+        std::vector<std::vector<seqan3::dna5>> parts = partition(q, 2); // does what it should
+     
+        for (auto p: parts){
+            // find occurrence in index without error
+            auto occ = seqan3::search(p, index, cfg);
+
+            for (auto &&pos: occ){
+                // TODO! if (verify(ref, query, pos +- ....something)):
+                // std::cout << "found something\n"
+            }
+        }
+    }
 
     return 0;
 }
